d38abfad739fbad4db07ffd56140ac3a3a208f64
Wellesley Shumway <wkshumway@Wellesleys-MacBook-Pro.local>
2017-09-22 12:49:42 -0700

    First Commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.jsx",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
+
+// source: mschwarzmueller on github
+// url: https://github.com/mschwarzmueller/reactjs-basics/blob/master/webpack.config.js
